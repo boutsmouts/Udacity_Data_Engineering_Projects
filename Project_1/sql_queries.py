8966af7e3ec345ce79490050d6977d786f6bed83
Marius <58073833+boutsmouts@users.noreply.github.com>
2020-09-02 11:40:37 +0200

    Update comments in code

--- Project_1/sql_queries.py
+++ Project_1/sql_queries.py
@@ -59,15 +59,20 @@ time_table_create       = ("""CREATE TABLE IF NOT EXISTS time (\
 songplay_table_insert   = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
                             VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s) \
                             ON CONFLICT (songplay_id) DO NOTHING \
-                        """)    # Note: I'm using the DEFAULT keyword to auto-assign an incrementing id when inserting a new row into the table.
-                                # Technically, I could've skipped the songplay_id in the first bracket statement to achieve the same thing without DEFAULT.
+                        """)
+
+# Note: I'm using the DEFAULT keyword to auto-assign an incrementing id when inserting a new row into the table.
+# Technically, I could've skipped the songplay_id in the first bracket statement to achieve the same thing without DEFAULT.
+
 
 user_table_insert       = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) \
                             VALUES (%s, %s, %s, %s, %s) \
                             ON CONFLICT (user_id) DO UPDATE SET \
                             level = EXCLUDED.level, \
                             last_name = EXCLUDED.last_name \
-                        """)    # Note: level might change due to upgrade of acccount, last_name might change due to marriage, which should both be taken into account for the users table.
+                        """)
+
+# Note: level might change due to upgrade of acccount, last_name might change due to marriage, which should both be taken into account for the users table.
 
 song_table_insert       = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) \
                             VALUES (%s, %s, %s, %s, %s) \
@@ -80,7 +85,9 @@ artist_table_insert     = ("""INSERT INTO artists (artist_id, name, location, la
                             location = EXCLUDED.location, \
                             latitude = EXCLUDED.latitude, \
                             longitude = EXCLUDED.longitude \
-                        """)    # Note: Artist location and coordinates might change, which should be taken into account.
+                        """)
+
+# Note: Artist location and coordinates might change, which should be taken into account.
 
 time_table_insert       = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) \
                             VALUES (%s, %s, %s, %s, %s, %s, %s) \
